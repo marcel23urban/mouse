f66659c1e7616348716f93f90c7658ce15d146c3
marcel23urban <marcel.urban23@gmx.de>
2025-07-08 07:16:04 +0200

    add some

--- baseprocessor.hpp
+++ baseprocessor.hpp
@@ -1,11 +1,11 @@
 #ifndef BASEPROCESSOR_HPP
 #define BASEPROCESSOR_HPP
 
+#include <thread>
+
 #include "conditionalsafequeue.hpp"
 
 class BaseProcessor {
-
-
     void run() {
         std::vector<std::complex<float>> data;
 
@@ -13,7 +13,6 @@ class BaseProcessor {
             if( _puff.try_pop( data)) {
                 process( data);
             }
-
         }
     }
 
@@ -24,13 +23,27 @@ public:
 
     }
 
-    void start() { _running = true;};
-    void stop() { _running = false;};
+    bool start() {
+        _running = true;
+        _fred = std::thread( &BaseProcessor::process, this);
+        return _fred.joinable();
+    };
+    bool stop() {
+        _running = false;
+
+    };
+
+    void dataIn( std::vector<std::complex<float>> input) {
+        if( input.empty()) return;
+        _puff.push( input);
+    }
+
 
 private:
 
     std::atomic<bool> _running;
     ConditionSafeQueue<std::complex<float>> _puff;
+    std::thread _fred;
 };
 
 #endif // BASEPROCESSOR_HPP
