b60c85ae783c020ba83c5b03170b0a3a1764f409
marcel23urban <marcel.urban23@gmx.de>
2025-09-20 07:53:25 +0200

    corrected

--- fft.hpp
+++ fft.hpp
@@ -9,7 +9,7 @@ class FFT {
     pocketfft::shape_t _shape;             // Shape of the transform
     const pocketfft::stride_t stride_in = {sizeof(std::complex<float>)};   // Stride for input array
     const pocketfft::stride_t stride_out = {sizeof( std::complex<float>)};  // Stride for output array
-    const pocketfft::stride_t stride_in_float = { sizeof( float)}
+    const pocketfft::stride_t stride_in_float = { sizeof( float)};
     pocketfft::shape_t axes = {0};              // Axes to transform
 public:
     FFT( uint64_t leng = 1024) : _shape( {leng}) {
@@ -35,7 +35,7 @@ public:
 
     /// @brief Computes vector of floats to its complex frequency domain, just one dimensional
     void fft( const float *input, std::complex<float> *output) {
-        pocketfft::r2c<flaot>( _shape, stride_in, stride_out, 1, pocketfft::FORWARD, input, output, 1.);
+        pocketfft::r2c<float>( _shape, stride_in, stride_out, 1, pocketfft::FORWARD, input, output, 1.);
     }
     /// @brief Computes vector of floats to its complex frequency domain
     void fft( const std::vector<float> &input, std::vector<std::complex<float>> &output) {
@@ -49,7 +49,7 @@ public:
     /// @param output fft with input leng
     void ifft( const std::complex<float> *input, std::complex<float> *output) {
         pocketfft::c2c<float>( _shape, stride_in, stride_out, axes, pocketfft::BACKWARD,
-                              input, output, 1. / static_cast<double>( leng));
+                              input, output, 1. / static_cast<double>( this->leng()));
     }
     /// @brief performs complex<float> to complex<float> reverse-fft
     /// @param input data of exact leng samples
